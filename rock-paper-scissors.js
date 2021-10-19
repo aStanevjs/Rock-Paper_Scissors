9e41b5ad8f8554019bdd331d2bb9b4f7bcd7d253
aStanevjs <atanasstanev@abv.bg>
2021-10-19 22:40:02 +0300

    first commit

---
+++ rock-paper-scissors.js
@@ -0,0 +1,50 @@
+const userChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "scissors" ||
+    userInput === "paper"
+  ) {
+    return userInput;
+  }
+};
+const computerChoice = function () {
+  do {
+    choice = Math.floor(Math.random() * 4);
+  } while (choice < 1);
+  switch (choice) {
+    case 1:
+      choice = "rock";
+      break;
+    case 2:
+      choice = "paper";
+      break;
+    case 3:
+      choice = "scissors";
+      break;
+  }
+  return choice;
+};
+
+const botChoice = computerChoice();
+
+function whoWins(userChoice, botChoice) {
+  if (
+    userChoice != "rock" &&
+    userChoice != "paper" &&
+    userChoice != "scissors"
+  ) {
+    console.log("Please enter a valid choice!!!");
+  } else if (userChoice === botChoice) {
+    console.log(`${userChoice} vs ${botChoice} \nDraw!`);
+  } else if (
+    (userChoice === "paper" && botChoice === "scissors") ||
+    (userChoice === "rock" && botChoice === "paper") ||
+    (userChoice === "scissors" && botChoice === "rock")
+  ) {
+    console.log(`${userChoice} vs ${botChoice} \nCrap, you lose!`);
+  } else {
+    console.log(`${userChoice} vs ${botChoice} \nCongratulations, you win!`);
+  }
+}
+whoWins(userChoice("Rock"), botChoice);
